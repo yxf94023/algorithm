50e779ad9e010645dfa552d7bcaa1fbc4b94b5bf
yxf94023 <1594149156@qq.com>
2017-04-03 22:27:27 +0800

    修改dijkstra中的一处错误

--- 图论/图_最小生成树(普里姆)/code/graph_prim.cc
+++ 图论/图_最小生成树(普里姆)/code/graph_prim.cc
@@ -1,12 +1,12 @@
 /******************************************************************************
- *\file graph_dijkstra.cc
- *\brief 图的最短路径
- *		迪杰斯特拉算法
+ *\file graph_prim.cc
+ *\brief 图的最小生成树
+ *		Prim算法
  *		图 采用 邻接表（Adjacency list） 形式存储
- *\date 2017/03/22
+ *\date 2017/04/03
  *****************************************************************************/
 
-#include "graph_dijkstra.h"
+#include "graph_prim.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
@@ -200,56 +200,55 @@ static void destroy_graph(adj_graph_st *pg)
 /**
  *\brief 调整边的辅助数组中边的状态
  *\param[in] pg 图的句柄
- *\param[in] dij_array 边的辅助数字
+ *\param[in] prim_array 边的辅助数字
  *\param[in] idx刚加入S集的顶点下标
  */
-static void adjust_arc_status(adj_graph_st *pg, dij_arc_st *dij_array, unsigned int idx)
+static void adjust_arc_status(adj_graph_st *pg, prim_arc_st *prim_array, unsigned int idx)
 {
-	dij_arc_st *p_ret = NULL;
+	prim_arc_st *p_ret = NULL;
 	unsigned int i = 0;
 	
 	for (i = 0; i < pg->arc_cnt; ++i){
 		
-		if ((dij_array[i].ver1 == idx || dij_array[i].ver2 == idx) && dij_array[i].status != 2){
+		if ((prim_array[i].ver1 == idx || prim_array[i].ver2 == idx) && prim_array[i].status != 2){
 			
-			if (dij_array[i].status == 0){
+			if (prim_array[i].status == 0){
 				
-				dij_array[i].status = 1;
-			}else if (dij_array[i].status == 1){
+				prim_array[i].status = 1;
+			}else if (prim_array[i].status == 1){
 				
-				dij_array[i].status = 2;
+				prim_array[i].status = 2;
 			}else{
 				
-				assert(2 > dij_array[i].status);
+				assert(2 > prim_array[i].status);
 			}
 		}
 	}
 }
  
 /**
- *\brief 找到和 S集合中 顶点相连的最短边
+ *\brief 找到和 S集合 顶点相连的最短边
  *\param[in] pg 图的句柄
- *\param[in] dij_array 边的辅助数字
- *\param[in] idx刚加入S集的顶点下标
+ *\param[in] prim_array 边的辅助数字
  *\retval 返回最短边的地址
  */
-static dij_arc_st *search_min_arc(adj_graph_st *pg, dij_arc_st *dij_array)
+static prim_arc_st *search_min_arc(adj_graph_st *pg, prim_arc_st *prim_array)
 {
-	dij_arc_st *p_ret = NULL;
+	prim_arc_st *p_ret = NULL;
 	unsigned int i = 0;
 	
 	for (i = 0; i < pg->arc_cnt; ++i){
 		
-		if (dij_array[i].status == 1){
+		if (prim_array[i].status == 1){
 			
 			if (NULL == p_ret){
 				
-				p_ret = &dij_array[i];
+				p_ret = &prim_array[i];
 			}else{
 				
-				if (p_ret->weight > dij_array[i].weight){
+				if (p_ret->weight > prim_array[i].weight){
 					
-					p_ret = &dij_array[i];
+					p_ret = &prim_array[i];
 				}
 			}
 		}
@@ -263,17 +262,29 @@ static dij_arc_st *search_min_arc(adj_graph_st *pg, dij_arc_st *dij_array)
 /**
  *\brief 输出dijkstra计算的最终结果
  *\param[in] pg 图的句柄
- *\param[in] dij_array 顶点的辅助数字
- *\param[in] idx 起始点 
+ *\param[in] prim_array 顶点的辅助数字
  */
-static void print_dijkstra_result(adj_graph_st *pg, dij_ver_st *dij_array, unsigned int idx)
+static void print_prim_result(adj_graph_st *pg, prim_ver_st *prim_array)
 {
-	unsigned int i = 0;
+	unsigned int i = 0, j = 0;
+	unsigned int total = 0;
 	
+	printf("\nresult: \n");
+	
+	// 对结果按照 加入最小生成树的先后顺序排序
 	for (i = 0; i < pg->ver_cnt; ++i){
 		
-		printf("%u - %u(%u)\n", idx, i, dij_array[i].weight);
-	}	
+		for (j = 0; j < pg->ver_cnt; ++j){
+			
+			if (prim_array[j].index == i + 1){
+				
+				printf("%u(%u) ", j, prim_array[j].weight);
+				total += prim_array[j].weight;
+			}
+		}
+	}
+
+	printf("\ntotal_weight[%u]\n", total);
 }
 
 /**
@@ -281,19 +292,19 @@ static void print_dijkstra_result(adj_graph_st *pg, dij_ver_st *dij_array, unsig
  *\param[in] pg 图的句柄
  *\param[in] sidx 起始搜索的顶点
  */
-static void dijkstra_graph(adj_graph_st *pg, unsigned int sidx)
+static void prim_graph(adj_graph_st *pg, unsigned int sidx)
 {
-	dij_ver_st *dij_ver_ary = NULL;
-	dij_arc_st *dij_arc_ary = NULL;
+	prim_ver_st *prim_ver_ary = NULL;
+	prim_arc_st *prim_arc_ary = NULL;
 	arc_st *p_tmparc = NULL;	
-	unsigned int i = 0, j = 0;
+	unsigned int i = 0, j = 0, index = 1;
 	
 	assert(NULL != pg);
 	assert(sidx < pg->ver_cnt);
 	
 	// 初始化辅助变量
-	dij_arc_ary = (dij_arc_st *)malloc(sizeof(dij_arc_st) * pg->arc_cnt);
-	assert(NULL != dij_arc_ary);
+	prim_arc_ary = (prim_arc_st *)malloc(sizeof(prim_arc_st) * pg->arc_cnt);
+	assert(NULL != prim_arc_ary);
 	for (i = 0; i < pg->ver_cnt; ++i){
 		
 		p_tmparc = pg->ver_list[i].p_arc;
@@ -301,10 +312,10 @@ static void dijkstra_graph(adj_graph_st *pg, unsigned int sidx)
 
 			if (i < p_tmparc->index){
 				
-				dij_arc_ary[j].ver1 = i;
-				dij_arc_ary[j].ver2 = p_tmparc->index;
-				dij_arc_ary[j].weight = p_tmparc->weight;
-				dij_arc_ary[j].status = 0;
+				prim_arc_ary[j].ver1 = i;
+				prim_arc_ary[j].ver2 = p_tmparc->index;
+				prim_arc_ary[j].weight = p_tmparc->weight;
+				prim_arc_ary[j].status = 0;
 				++j;
 			}
 			
@@ -312,50 +323,44 @@ static void dijkstra_graph(adj_graph_st *pg, unsigned int sidx)
 		}
 	}// 这里其实可以采用堆排序 加速后面的查找过程
 	
-	dij_ver_ary = (dij_ver_st *)malloc(sizeof(dij_ver_st) * pg->ver_cnt);
-	assert(NULL != dij_ver_ary);
+	prim_ver_ary = (prim_ver_st *)malloc(sizeof(prim_ver_st) * pg->ver_cnt);
+	assert(NULL != prim_ver_ary);
 	for (i = 0; i < pg->ver_cnt; ++i){
 		
-		dij_ver_ary[i].weight = DIJ_MAX_WEIGHT;
-		dij_ver_ary[i].isfinal = 0;
+		prim_ver_ary[i].index = 0;
+		prim_ver_ary[i].weight = 0;
+		prim_ver_ary[i].isfinal = 0;
 	}
 	
-	dij_ver_ary[sidx].weight = 0;
-	dij_ver_ary[sidx].isfinal = 1;
-	adjust_arc_status(pg, dij_arc_ary, sidx);
+	prim_ver_ary[sidx].index = index++;
+	prim_ver_ary[sidx].weight = 0;
+	prim_ver_ary[sidx].isfinal = 1;
+	adjust_arc_status(pg, prim_arc_ary, sidx);
 	
 	for (i = 1; i < pg->ver_cnt; ++i){
 
-		// 调整顶点的距离
-		p_tmparc = pg->ver_list[sidx].p_arc;
-		while (NULL != p_tmparc){
-			
-			if (dij_ver_ary[p_tmparc->index].weight > dij_ver_ary[sidx].weight + p_tmparc->weight){
-				
-				dij_ver_ary[p_tmparc->index].weight = dij_ver_ary[sidx].weight + p_tmparc->weight;
-			}
-			
-			p_tmparc = p_tmparc->p_next;
-		}
-
 		// 查找和S集合连接的最小边
-		dij_arc_st *dij_arc = NULL;
-		dij_arc = search_min_arc(pg, dij_arc_ary);
+		prim_arc_st *prim_arc = NULL;
+		prim_arc = search_min_arc(pg, prim_arc_ary);
 		
-		if (!dij_ver_ary[dij_arc->ver1].isfinal){
+		if (!prim_ver_ary[prim_arc->ver1].isfinal){
 			
-			sidx = dij_arc->ver1;
-			dij_ver_ary[dij_arc->ver1].isfinal = 1;
+			sidx = prim_arc->ver1;
+			prim_ver_ary[prim_arc->ver1].isfinal = 1;
+			prim_ver_ary[prim_arc->ver1].index = index++;
+			prim_ver_ary[prim_arc->ver1].weight = prim_arc->weight;
 		}else{
 		
-			sidx = dij_arc->ver2;
-			dij_ver_ary[dij_arc->ver2].isfinal = 1;
+			sidx = prim_arc->ver2;
+			prim_ver_ary[prim_arc->ver2].isfinal = 1;
+			prim_ver_ary[prim_arc->ver2].index = index++;
+			prim_ver_ary[prim_arc->ver2].weight = prim_arc->weight;
 		}
-		adjust_arc_status(pg, dij_arc_ary, sidx);		
+		adjust_arc_status(pg, prim_arc_ary, sidx);		
 	}
 	
 	// 输出所有的具体信息
-	print_dijkstra_result(pg, dij_ver_ary, 0);
+	print_prim_result(pg, prim_ver_ary);
 }
 
 int main(void)
@@ -366,7 +371,7 @@ int main(void)
 	
 	print_graph(&graph);
 	
-	dijkstra_graph(&graph, 0);
+	prim_graph(&graph, 0);
 	
 	destroy_graph(&graph);
 	
---
+++ 图论/图_最小生成树(普里姆)/code/graph_prim.h
@@ -0,0 +1,78 @@
+/******************************************************************************
+ *\file graph_prim.h
+ *\brief 定义图相关的结构体
+ *\date 2017/04/03
+ *****************************************************************************/
+#ifndef __GRAPH_PRIM_H_2017_04_03__
+#define	__GRAPH_PRIM_H_2017_04_03__
+
+
+#define VER_CHAR_SIZE	(10)	///< 结点的字符串长度
+
+typedef char ver_type[VER_CHAR_SIZE];	///< 定义节点类型
+
+/**
+ *\brief 图中 弧（边）的结构
+ *
+ *	- 两个成员变量 分别是
+ *		+ 图顶点 在顶点向量中的下标值，值是从0下标开始的
+ *		+ 指向下一个弧（边）的指针
+ */
+typedef struct arc_st{
+	
+	unsigned int	index;		///< 顶点的下标值
+	unsigned int	weight;		///< 权重
+	struct arc_st	*p_next;	///< 指向下一个弧
+}arc_st;
+
+/**
+ *\brief 图中 顶点 的结构
+ *
+ *	- 采用邻接表的方式存储，含有两个成员变量
+ *		+ 顶点的名称
+ *		+ 指向下一个弧（边）的指针
+ */
+typedef struct ver_st{
+	
+	ver_type	ver_name;	///< 顶点名称
+	arc_st		*p_arc;		///< 弧（边）
+}ver_st;
+
+/**
+ *\brief 图的结构
+ */
+typedef struct adj_graph_st{
+	
+	unsigned int	arc_cnt;	///< 图中 边的数量
+	unsigned int	ver_cnt;	///< 图中 结点的数量
+	
+	ver_st			*ver_list;	///< 顶点 列表   动态数组
+}adj_graph_st;
+
+/**
+ *\brief 辅助结构体 实现prim算法
+ *
+ *	记录选取的顺序
+ *	标记顶点是否已经标记过
+ */
+typedef struct prim_ver_st{
+
+	unsigned int 	index;		///< 顶点被标记的顺序位置
+	unsigned int	weight;		///< 选择加入这个顶点的权重边
+	unsigned int 	isfinal;	///< 是否被标记过默认为0未标记，1表示标记
+}prim_ver_st;
+
+/**
+ *\brief 辅助结构体 实现prim算法
+ *
+ *	记录边，对边进行排序
+ */
+typedef struct prim_arc_st{
+	
+	unsigned int	ver1;		///< 边上顶点1
+	unsigned int 	ver2;		///< 边上顶点2
+	unsigned int 	weight;		///< 边的权重
+	unsigned int 	status;		///< 边的状态 0,1,2
+}prim_arc_st;
+
+#endif//__GRAPH_PRIM_H_2017_04_03__
