f21088150a7004cad7b4ea622246ad7d0517061b
yxf94023 <1594149156@qq.com>
2017-04-11 01:09:21 +0800

    提交归并排序代码

--- 排序/归并排序/code/merge_sort.cc
+++ 排序/归并排序/code/merge_sort.cc
@@ -5,11 +5,13 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 #include <assert.h>
 
 static int s_len = 0;		///< 待排序的数列长度
 static int s_ary[10000];	///< 存放数列
+static int s_buf[10000];	///< 临时交换区
 
 /** 
  *\brief 根据输入 随机创建
@@ -27,7 +29,7 @@ static void creat_ary(void)
 	
 	for (i = 0; i < s_len; ++i){
 			
-		s_ary[i] = rand();
+		s_ary[i] = rand()%10000;
 	}
 }
 
@@ -42,14 +44,68 @@ static void print_ary(void)
 		
 		printf("%d ", s_ary[i]);
 	}
+	printf("\n");
 }
 
 /**
  *\brief 归并排序
+ *\param[in] beg 起始位置下标
+ *\param[in] end 结束位置下标
  */
-static void merge_sort()
+static void merge_sort(int beg, int end)
 {
+	int mid = 0;
+	int i = 0, j = 0, k = 0;
 	
+	assert (end >= beg);
+	
+	if (end - beg > 1){
+		
+		mid = (end - beg) / 2;
+		merge_sort(beg, beg + mid);
+		merge_sort(beg + mid + 1, end);
+		
+		// 归并 [beg, beg + mid]  和 [beg + mid + 1, end] 两个有序单元
+		for (i = beg, j = beg + mid + 1, k = beg; i <= beg + mid && j <= end; ++k){
+			
+			if (s_ary[i] < s_ary[j]){
+				
+				s_buf[k] = s_ary[i];
+				++i;
+			}else{
+				
+				s_buf[k] = s_ary[j];
+				++j;
+			}
+		}
+		
+		while (i <= beg + mid){	// 将剩余的全部拷贝
+			
+			s_buf[k] = s_ary[i];
+			++k;
+			++i;
+		}
+		
+		while(j <= end){	// 将剩余的全部拷贝
+			
+			s_buf[k] = s_ary[j];
+			++k;
+			++j;
+		}
+		memcpy(&s_ary[beg], &s_buf[beg], (end - beg + 1) * sizeof(int));
+	}else if (end - beg == 1){
+		
+		// 两个, 直接交换
+		if (s_ary[beg] > s_ary[end]){
+			
+			s_ary[beg] += s_ary[end];
+			s_ary[end] = s_ary[beg] - s_ary[end];
+			s_ary[beg] = s_ary[beg] - s_ary[end];
+		}
+	}else{
+		
+		// end == beg 表示只有一个， 不处理
+	}
 }
 
 int main(int argc, char **argv)
@@ -58,7 +114,7 @@ int main(int argc, char **argv)
 	
 	print_ary();
 	
-	merge_sort();
+	merge_sort(0, s_len - 1);
 	
 	print_ary();
 	
