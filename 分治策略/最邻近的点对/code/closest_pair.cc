55d3fda71f037df1579b9fcf2d3fc9d4eba98fd9
yxf94023 <1594149156@qq.com>
2017-04-22 01:26:17 +0800

    保存代码

--- 分治策略/最邻近的点对/code/closest_pair.cc
+++ 分治策略/最邻近的点对/code/closest_pair.cc
@@ -8,21 +8,77 @@
 #include <assert.h>
 #include <math.h>
 
-
-static int __do_closest_pair(point_st *plist, unsigned int beg, unsigned int num, closest_pair_st *pair)
+static ptables_st x_ptables[MAX_POINTS];	// x轴排序列表
+static ptables_st y_ptables[MAX_POINTS];	// y轴排序列表
+static unsigned int s_spnum;
+static ptables_st s_ptables[MAX_POINTS];	// 与垂直线相距特定距离点的列表
+	
+/**
+ *\brief 获取两点间的距离
+ *\brief[in] plist 点的列表
+ *\param[in] i 第一个点在点的列表中下标值
+ *\param[in] j 第二个点在点的列表中下标值
+ *\retval 2点间的距离值
+ */
+static double __get_2points_dist(point_st *plist, unsigned int i, unsigned int j)
 {
+    double x, y;
+    
+    x = pow(plist[i].x - plist[j].x, 2.0);
+    y = pow(plist[i].y - plist[j].y, 2.0);
+    
+    return sqrt(x + y);
+}
+
+/**
+ *\brief 递归处理各个分支
+ *\param[in] plist 存放所有点列表
+ *\param[in] pnum plist列表中点的数量
+ *\param[in] beg 待处理点的起始下标
+ *\param[in] num 待处理点的数量 从beg下标开始算起
+ *\param[out] pair 输出邻近的点对
+ *\retval 1 成功
+ *\retval 0 失败
+ */
+static int __do_closest_pair(point_st *plist, 
+							 unsigned int pnum,
+							 unsigned int beg, 
+							 unsigned int num, 
+							 closest_pair_st *pair)
+{
+	unsigned int i = 0, j = 0;
 	closest_pair_st l_pair, r_pair, min_pair;
 	
 	if (num <= 3){
 		
+		pair->p1.x = plist[beg].x;
+		pair->p1.y = plist[beg].y;
+		pair->p2.x = plist[beg+1].x;
+		pair->p2.y = plist[beg+1].y;
+		pair->d = __get_2points_dist(plist, beg, beg + 1);
+		
 		// 直接计算两点间的距离
+		for (i = 0; i < num; ++i){
+			
+			for (j = i + 1; j < num; ++j){
+				
+				if (pair->d > __get_2points_dist(plist, beg + i, beg + j)){
+					
+					pair->p1.x = plist[beg+i].x;
+					pair->p1.y = plist[beg+i].y;
+					pair->p2.x = plist[beg+j].x;
+					pair->p2.y = plist[beg+j].y;
+					pair->d = __get_2points_dist(plist, beg + i, beg + j);					
+				}
+			}
+		}
 		
 		return 1;
 	}
 	
-	// 拆分成[1, pnum/2] 和 [pnum/2 + 1, pnum]
-	__do_closest_pair(beg, pnum/2, &l_pair);
-	__do_closest_pair(beg + pnum/2, pnum - pnum/2, &r_pair);
+	// 拆分成[1, num/2] 和 [num/2 + 1, num]
+	__do_closest_pair(plist, beg, num/2, &l_pair);
+	__do_closest_pair(plist, beg + num/2, num - num/2, &r_pair);
 	if (l_pair.d < r_pair.d){
 		
 		memset(&min_pair, &l_pair, sizeof(closest_pair_st));
@@ -31,10 +87,50 @@ static int __do_closest_pair(point_st *plist, unsigned int beg, unsigned int num
 		memset(&min_pair, &r_pair, sizeof(closest_pair_st));
 	}
 	
-	// 计算两点一点落在左半边 一点落在右半边的情况
+	// 计算两点一点落在左半边 一点落在右半边的情况， 统计所有与垂直直线 x = plist[beg + num/2].x 相距 min_pair.d 的距离的点
+	for (i = 0, s_spnum = 0; i < pnum ; ++i){
+		
+		double dis = fabs(plist[beg + num/2].x - plist[y_ptables[i].idx].x);
+		if (min_pair.d > dis){
+			
+			s_ptables[s_spnum].idx = y_ptables[i].idx;
+			s_ptables[s_spnum].xy = y_ptablse[i].xy;
+			++s_spnum;
+		}
+	}
 	
+	if (s_spnum < 2){
 	
+		memset(pair, &min_pair, sizeof(closest_pair_st));
+		return 1;
+	}
 	
+	pair->p1.x = plist[s_ptables[0].idx].x;
+	pair->p1.y = plist[s_ptables[0].idx].y;
+	pair->p2.x = plist[s_ptables[1].idx].x;
+	pair->p2.y = plist[s_ptables[1].idx].y;
+	pair->d = __get_2points_dist(plist, s_ptables[0].idx, s_ptables[1].idx);
+	for (i = 0; i < s_spnum; ++i){
+		
+		for (j = i+1; j <= i + 8 && j < s_spnum; ++j){
+			
+			if (pair->d > __get_2points_dist(plist, s_ptables[i].idx, s_ptables[j].idx)){
+				
+				pair->p1.x = plist[s_ptables[i].idx].x;
+				pair->p1.y = plist[s_ptables[i].idx].y;
+				pair->p2.x = plist[s_ptables[j].idx].x;
+				pair->p2.y = plist[s_ptables[j].idx].y;
+				pair->d = __get_2points_dist(plist, s_ptables[i].idx, s_ptables[j].idx);				
+			}		
+		}
+	}
+	
+	if (min_pair.d < pair->d){
+		
+		memset(pair, &min_pair, sizeof(closest_pair_st));
+	}
+	
+	return 1;
 }
 
 /**
@@ -47,8 +143,6 @@ static int __do_closest_pair(point_st *plist, unsigned int beg, unsigned int num
  */
 int get_closest_pair(point_st *plist, unsigned int pnum, closest_pair_st *pair)
 {
-	ptables_st x_ptables[MAX_POINTS];	// x轴排序列表
-	ptables_st y_ptables[MAX_POINTS];	// y轴排序列表
 	unsigned int i = 0;
 	
 	
@@ -67,7 +161,7 @@ int get_closest_pair(point_st *plist, unsigned int pnum, closest_pair_st *pair)
 	// 调用归并排序
 	
 	// 2.查找最小临近点对
-	__do_closest_pair(plist, 0, pnum, pair);
+	__do_closest_pair(plist, pnum, 0, pnum, pair);
 	
 	return 0;
 }
---
+++ 分治策略/最邻近的点对/code/merge_sort.cc
@@ -0,0 +1,166 @@
+/**
+ *\file merge_sort.cc
+ *\brief 归并排序
+ *\date 2017/04/10
+ */
+
+#include "merge_sort.h"
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+
+static void **s_merge_array = (void**)0;	///< 交换的变量
+static void **s_merge_swap = (void**)0;	///< 临时交换区
+static unsigned int merge_len = 0;	///< 待排序的规模
+static merge_cmp_fun mcmp_fun = NULL;	///< 对比函数
+static merge_agn_fun magn_fun = NULL;	///< 赋值函数
+static merge_set_fun mset_fun = NULL;	///< 设置merge_array的初始值
+
+/**
+ *\brief 初始化归并排序环境
+ *\param[in] len 待排序的规模
+ *\param[in] disorder_array 待排序的数组
+ *\param[in] f_set 设置merge_array初始值函数
+ *\param[in] f_cmp 排序过程中需要的比较函数
+ *\param[in] f_agn 赋值操作函数
+ *\retval 1 初始化成功
+ *\retval 0 初始化失败
+ */
+int merge_init(unsigned int len, void *disorder_array,
+				merge_set_fun f_set, 
+				merge_cmp_fun f_cmp, 
+				merge_agn_fun f_agn)
+{
+	assert(len > 1);
+	assert((void**)0 != disorder_array);	
+	assert(NULL != f_set);
+	assert(NULL != f_set);
+	assert(NULL != f_set);	
+	
+	s_merge_array = (void **) malloc (sizeof(void*) * len);
+	s_merge_swap = (void **) malloc (sizeof(void*) * len);
+	if ((void**)0 == s_merge_array || (void**)0 == s_merge_swap){
+		
+		printf("malloc failed errno(%d)!\n", errno);
+		if (s_merge_array){
+			
+			free(s_merge_array);
+			s_merge_array = (void**)0;
+		}
+		
+		if (s_merge_swap){
+			
+			free(s_merge_swap);
+			s_merge_swap = (void**)0;			
+		}
+		
+		return 0;
+	}
+	merge_len = len;
+	
+	mset_fun = f_set;
+	mcmp_fun = f_cmp;
+	magn_fun = f_agn;
+	
+	return mset_fun(len, disorder_array, s_merge_array);
+}
+
+/**
+ *\brief 执行 归并排序
+ *\param[in] beg 起始位置下标
+ *\param[in] end 结束位置下标
+ */
+static void __msort(unsigned int beg, unsigned int end)
+{
+	unsigned int mid = 0;
+	unsigned int i = 0, j = 0, k = 0;
+	int ret = 0;
+	
+	assert (end >= beg);
+	
+	if (end - beg >= 1){
+		
+		mid = (end - beg) / 2;
+		__msort(beg, beg + mid);
+		__msort(beg + mid + 1, end);
+		
+		// 归并 [beg, beg + mid]  和 [beg + mid + 1, end] 两个有序单元
+		for (i = beg, j = beg + mid + 1, k = beg; i <= beg + mid && j <= end; ++k){
+			
+			ret = mcmp_fun(s_merge_array[i], s_merge_array[j]);
+			if (-1 == ret){
+				
+				magn_fun(&s_merge_swap[k], s_merge_array[i]);
+				++i;
+			}else{
+				
+				magn_fun(&s_merge_swap[k], s_merge_array[j]);
+				++j;
+			}
+		}
+		
+		while (i <= beg + mid){	// 将剩余的全部拷贝
+			
+			magn_fun(&s_merge_swap[k], s_merge_array[i]);
+			++k;
+			++i;
+		}
+		
+		while(j <= end){	// 将剩余的全部拷贝
+			
+			magn_fun(&s_merge_swap[k], s_merge_array[j]);
+			++k;
+			++j;
+		}
+		memcpy(&s_merge_array[beg], &s_merge_swap[beg], (end - beg + 1) * sizeof(void*));
+	}else{
+		
+		// end == beg 表示只有一个， 不处理
+	}	
+}
+
+/**
+ *\brief 归并排序
+ *\param[in] len 获取排序结果结果规模
+ *\param[in,out] res_array 获取排序结果
+ *\param[in] f_res 获取结果的回调
+ */
+void merge_sort(unsigned int len, void *res_array, merge_res_fun f_res)
+{
+	__msort(0, merge_len - 1);
+	
+	if (len <= 0 || NULL == res_array || NULL == f_res){// 不期望返回结果
+	
+		return;
+	}
+	
+	len = len > merge_len ? merge_len : len;	// 取小的
+	f_res(len, res_array, s_merge_array);
+}
+
+/**
+ *\brief 销毁
+ */
+void merge_destory(void)
+{
+	if (s_merge_array){
+		
+		free(s_merge_array);
+		s_merge_array = (void**)0;
+	}
+	
+	if (s_merge_swap){
+		
+		free(s_merge_swap);
+		s_merge_swap = (void**)0;			
+	}	
+	
+	merge_len = 0;
+	
+	mset_fun = NULL;
+	mcmp_fun = NULL;
+	magn_fun = NULL;
+}
